b0a2b71e4d416cb683009af5377f8abdf972f6bb
Nilay Gupta <nilaygupta@Nilays-MacBook-Air.local>
2024-01-29 15:16:28 +0530

    minor updates on sample file

--- sample.java
+++ sample.java
@@ -1,7 +1,9 @@
 package GITHUB.Matrix_Class;
 import java.util.Arrays;
-/*this is a sample programme that displays how you can use different fucntions of this matrix class */
-/*Class valid of 3*3 matrix Only. */
+/*this is a sample programme that displays how you can use different fucntions of this matrix class 
+ Class valid for 3*3 type matrix Only.
+Objects are initialised as integer matrix, where as arguments in inbuilt functions can be passed as float or int types matrices.*/
+
 public class sample {
     public static void main(String [] args){
         int [][]n ={{1,0,0},{0,1,0},{0,0,1}};
@@ -11,7 +13,8 @@ public class sample {
         System.out.println(Arrays.deepToString(M1.subract(n)));
         System.out.println(Arrays.deepToString(M1.adjoint()));
         System.out.println((M1.determinant()));
-        int  [][]p1 ={{1,9,31},{3,10,6},{11,8,9}};
+        int  [][]p1 ={{1,9,31},{3,-10,6},{11,8,9}};
+        System.out.println(" ");
         System.out.println(Arrays.deepToString(M1.multiply(p1)));
         Matrix M2= new Matrix(p1); //initialising p1 as an object M2
         System.out.println(Arrays.deepToString(M2.adjoint()));
