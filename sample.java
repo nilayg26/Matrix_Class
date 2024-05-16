f4c51b80a94c83273d04a91a6928adf7591f8d67
Nilay Gupta <nilaygupta@Nilays-MacBook-Air.local>
2024-05-17 03:50:05 +0530

    minor changes

--- sample.java
+++ sample.java
@@ -1,11 +1,15 @@
 package GITHUB.Matrix_Class; //remove this line before running the code
-import java.util.Arrays;
+//Add the following line of code at the beginning, when using the Matrix class in different folders/files
+//import Folder_Name.Matrix_Class.Matrix;
+//'Folder_Name' is the name of the folder
+// in which you have cloned this repository.
+
 /*this is a sample programme that displays how you can use different fucntions of this matrix class 
  Class valid for 3*3 type matrix Only.
 Objects are initialised as integer matrix only, where as arguments in inbuilt functions can be passed as float or int types matrices.
 Output is given as float type 2D array. 
 Arrays.deepToString() Returns a string representation of the "deep contents" of a multidimensional array*/
-
+import java.util.Arrays;
 public class sample {
     public static void main(String [] args){
         int [][]n ={{1,0,0},{0,1,0},{0,0,1}};
