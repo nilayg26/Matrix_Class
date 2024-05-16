a081a941c0d3fb6f18c30d4d3872966a46e48b92
Nilay Gupta <nilaygupta@Nilays-MacBook-Air.local>
2024-05-17 03:43:16 +0530

    Revising some oops concepts

--- Matrix.java
+++ Matrix.java
@@ -1,134 +1,154 @@
-package GITHUB.Matrix_Class; //remove this line before running the code
-public class Matrix{
-int [][]key_matrix;
-int det =0;
-int cf=0;
-float [][] matrix_B= new float[3][3];
-public Matrix(int[][]key_matrix){
-    this.key_matrix=key_matrix;
-}
-public float [][] inverted(){ //returns the inverted matrix in [][] format
-    float[][] new_matrix=new float [3][3];
-    int det= determinant();
-    for(int i=0;i<=2;i++){
-        for(int j=0;j<=2;j++){
-            new_matrix[i][j]=(((float)cofactor(i, j)/(float)det));
+package GITHUB.Matrix_Class;
+//replace the above line with:
+//package Folder_Name.Matrix_Class;
+//'Folder_Name' is the name of the folder
+// in which you have cloned this repository. 
+public class Matrix {
+    private int[][] key_matrix;
+    private int det;
+    private int cf;
+    private float[][] matrix_B;
+
+    public Matrix(int[][] key_matrix) {
+        this.key_matrix = key_matrix;
+        det = 0;
+        cf = 0;
+        matrix_B = new float[3][3];
+    }
+
+    public float[][] inverted() { // returns the inverted matrix in [][] format
+        float[][] new_matrix = new float[3][3];
+        int det = determinant();
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++) {
+                new_matrix[i][j] = (((float) cofactor(i, j) / (float) det));
+            }
         }
-    }
-    float [][] final_matrix =new float[3][3];
-    for(int i=0;i<=2;i++){
-        for(int j=0;j<=2;j++){
-            final_matrix[i][j]=new_matrix[j][i];
+        float[][] final_matrix = new float[3][3];
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++) {
+                final_matrix[i][j] = new_matrix[j][i];
+            }
         }
+        return final_matrix;
     }
-    return final_matrix;
-}
-public float[][] adjoint(){ //returns the adjoint matrix in [][] format 
-    float[][] new_matrix=new float [3][3];
-    for(int i=0;i<=2;i++){
-        for(int j=0;j<=2;j++){
-            new_matrix[i][j]=(float)cofactor(i, j);
+
+    public float[][] adjoint() { // returns the adjoint matrix in [][] format
+        float[][] new_matrix = new float[3][3];
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++) {
+                new_matrix[i][j] = (float) cofactor(i, j);
+            }
         }
-    }
-    float [][] final_matrix =new float[3][3];
-    for(int i=0;i<=2;i++){
-        for(int j=0;j<=2;j++){
-            final_matrix[i][j]=new_matrix[j][i];
+        float[][] final_matrix = new float[3][3];
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++) {
+                final_matrix[i][j] = new_matrix[j][i];
+            }
         }
+        return final_matrix;
     }
-    return final_matrix;
-}
-public float [][] transpose(){ //returns the transpose matrix in [][] format
-    float [][] final_matrix =new float[3][3];
-    for(int i=0;i<=2;i++){
-        for(int j=0;j<=2;j++){
-            final_matrix[i][j]=key_matrix[j][i];
+
+    public float[][] transpose() { // returns the transpose matrix in [][] format
+        float[][] final_matrix = new float[3][3];
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++) {
+                final_matrix[i][j] = key_matrix[j][i];
+            }
         }
+        return final_matrix;
     }
-    return final_matrix;
-}
-int cofactor(int i,int j){
-    cf= (key_matrix[(i+1)%3][(j+1)%3]*key_matrix[(i+2)%3][(j+2)%3])
-        -(key_matrix[(i+2)%3][(j+1)%3]*key_matrix[(i+1)%3][(j+2)%3]);
-    return cf;
-}
-public int determinant(){  //returns the determinant of the matrix in int format
-    det=0;
-    for(int j=0;j<3;j++){
-        det=det+(key_matrix[0][j]*cofactor(0, j));
+
+    private int cofactor(int i, int j) {
+        cf = (key_matrix[(i + 1) % 3][(j + 1) % 3] * key_matrix[(i + 2) % 3][(j + 2) % 3])
+                - (key_matrix[(i + 2) % 3][(j + 1) % 3] * key_matrix[(i + 1) % 3][(j + 2) % 3]);
+        return cf;
     }
-    return det;
-}
-public float[][] add(int [][] matrix_C){
-    float [][] result = new float[3][3];
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            result[i][j]= (float)(matrix_C[i][j]+key_matrix[i][j]);
+
+    public int determinant() { // returns the determinant of the matrix in int format
+        det = 0;
+        for (int j = 0; j < 3; j++) {
+            det = det + (key_matrix[0][j] * cofactor(0, j));
         }
+        return det;
     }
-    return result;
-}
-public float[][] subtract(int [][] matrix_C){
-    float [][] result = new float[3][3];
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            result[i][j]= (float)(key_matrix[i][j]-matrix_C[i][j]);
+
+    public float[][] add(int[][] matrix_C) {
+        float[][] result = new float[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = (float) (matrix_C[i][j] + key_matrix[i][j]);
+            }
         }
+        return result;
     }
-    return result;
-}
-public float[][] add(float [][] matrix_C){
-    float [][] result = new float[3][3];
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            result[i][j]= matrix_C[i][j]+key_matrix[i][j];
+
+    public float[][] subtract(int[][] matrix_C) {
+        float[][] result = new float[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = (float) (key_matrix[i][j] - matrix_C[i][j]);
+            }
         }
+        return result;
     }
-    return result;
-}
-public float[][] subtract(float [][] matrix_C){
-    float [][] result = new float[3][3];
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            result[i][j]= key_matrix[i][j]-matrix_C[i][j];
+
+    public float[][] add(float[][] matrix_C) {
+        float[][] result = new float[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = matrix_C[i][j] + key_matrix[i][j];
+            }
         }
+        return result;
     }
-    return result;
-}
-public float [][] multiply(float [][] matrix_B){
-    this.matrix_B=matrix_B;
-    float [][] final_matrix =new float[3][3];
-    for(int j=0;j<=2;j++){
-        for(int i=0;i<=2;i++){
-            final_matrix[i][j]=multiply_helper(i, j);
+
+    public float[][] subtract(float[][] matrix_C) {
+        float[][] result = new float[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = key_matrix[i][j] - matrix_C[i][j];
+            }
         }
+        return result;
     }
-    return final_matrix;
-}
-public float [][] multiply(int [][] matrix_B){
-    for (int i = 0;i<3; i++) {
-        for(int j=0;j<3;j++){
-        this.matrix_B[i][j] = (float)matrix_B[i][j]; // Cast each element to int
-    }}
-    float [][] final_matrix =new float[3][3];
-    for(int j=0;j<=2;j++){
-        for(int i=0;i<=2;i++){
-            final_matrix[i][j]=multiply_helper(i, j);
+
+    public float[][] multiply(float[][] matrix_B) {
+        this.matrix_B = matrix_B;
+        float[][] final_matrix = new float[3][3];
+        for (int j = 0; j <= 2; j++) {
+            for (int i = 0; i <= 2; i++) {
+                final_matrix[i][j] = multiply_helper(i, j);
+            }
         }
+        return final_matrix;
     }
-    return final_matrix;
-}
-float multiply_helper(int i,int j){
-    float element=0;
-    for(int row=0;row<=2;row++){
-        element=element+(key_matrix[i][row]*matrix_B[row][j]);
+
+    public float[][] multiply(int[][] matrix_B) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                this.matrix_B[i][j] = (float) matrix_B[i][j]; // Cast each element to int
+            }
+        }
+        float[][] final_matrix = new float[3][3];
+        for (int j = 0; j <= 2; j++) {
+            for (int i = 0; i <= 2; i++) {
+                final_matrix[i][j] = multiply_helper(i, j);
+            }
+        }
+        return final_matrix;
     }
-    if(Math.abs(element)<0.001f){
-        element=0;
+
+    private float multiply_helper(int i, int j) {
+        float element = 0;
+        for (int row = 0; row <= 2; row++) {
+            element = element + (key_matrix[i][row] * matrix_B[row][j]);
+        }
+        if (Math.abs(element) < 0.001f) {
+            element = 0;
+        } else if (Math.abs(element) > 0.991 && Math.abs(element) < 1.001f) {
+            element = 1;
+        }
+        return element;
     }
-    else if(Math.abs(element)>0.991&&Math.abs(element)<1.001f){
-        element=1;
-    }
-    return element;
-}
 }
